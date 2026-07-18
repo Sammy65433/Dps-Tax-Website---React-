33afa1a0432b754f70b2cd8a595f6ef5bc2bdd2a
Sammy65433 <samiamlistening@gmail.com>
2026-07-17 22:45:37 -0400

    DPS TAX 101 bac-front 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
           <a href="#faq">FAQ</a>
           <a href="#testimonials">Testimonials</a>
           <a href="#booking" className="nav-cta">Book Now</a>
-          <a href="tel:+19735551234" className="nav-cta">Call the Office</a>
+          <a href="tel:+19735551234" className="nav-call">Call Now</a>
 
         </nav>
 
